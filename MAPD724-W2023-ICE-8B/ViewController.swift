5a0802868b1411d59585376daec7d671e60d3a8a
RazielHernandez <razo.puma.azteca@gmail.com>
2023-03-25 23:47:06 -0400

    First commit. Project created

---
+++ MAPD724-W2023-ICE-8B/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MAPD724-W2023-ICE-8B
+//
+//  Created by Raziel Hernandez on 2023-03-25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
